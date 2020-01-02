00760489402f11dd3c4b55a192f205b0fa2bfacb
IgorRamosBR <igorir7@gmail.com>
2020-01-02 11:58:30 -0300

    Tela de listas de compras

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack';
 import ShoppingListsScreen from './src/screens/ShoppingListsScreen'
 import ProductsScreen from "./src/screens/ProductsScreen";
 
+export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
+    return <AppContainer />
+  }
+}
 const ShoppingListsStack = createStackNavigator(
   {
     ShoppingLists: { 
@@ -100,6 +128,6 @@ const getTabBarIcon = (navigation, focused, tintColor) => {
 };
 
 
-export default createAppContainer(
+const AppContainer = createAppContainer(
   MainTabsStack
 );
--- src/screens/ShoppingListsScreen.js
+++ src/screens/ShoppingListsScreen.js
@@ -1,20 +1,85 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
+import { Dimensions } from 'react-native';
 
 export default class ShoppingListsScreen extends React.Component {
 
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shoppingLists: [
+        {
+          id: 0,
+          name: 'Compras de mês',
+          market: {
+            name: 'Guanabara',
+            logo: 'https://acgnews.com.br/wp-content/uploads/2018/02/guanabara.jpg'
+          },
+          date: '01/03/2019',
+          totalPrice: 'R$100,00'
+        },
+        {
+          id: 1,
+          name: 'Carnes',
+          market: {
+            name: 'Assaí',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Assai.svg/1280px-Assai.svg.png'
+          },
+          date: '15/03/2019',
+          totalPrice: 'R$100,00'
+        },
+        {
+          id: 2,
+          name: 'Compras de mês',
+          market: {
+            name: 'Guanabara',
+            logo: 'https://acgnews.com.br/wp-content/uploads/2018/02/guanabara.jpg'
+          },
+          date: '01/04/2019',
+          totalPrice: 'R$100,00'
+        }
+      ]
+    }
+
+  }
 
 
-    render() {
-        console.log('passou aqui ');
-
-        return (
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Lista de Compras!</Text>
-          </View>
-        );
-      }
-} 
+  render() {
+    return (
+      <Container>
+        <Content>
+          {this.state.shoppingLists.map((item, index) => {
+            return (
+              <Card key={index}>
+                <CardItem>
+                  <CardItem cardBody>
+                    <Image source={{ uri: item.market.logo }} style={{ height: 200, width: null, flex: 1 }} />
+                  </CardItem>
+                  <Left>
+                    <Body>
+                      <Text>{item.name}</Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                <CardItem>
+                  <Left>
+                    <Body>
+                      <Text style={{ fontSize: 20, fontWeight: '700' }}>Compras de Mês</Text>
+                      <Text note>{item.date}</Text>
+                    </Body>
+                  </Left>
+                  <Right>
+                    <Text>{item.totalPrice}</Text>
+                  </Right>
+                </CardItem>
+              </Card>
+            )
+          })}
+        </Content>
+      </Container>
+    );
+  }
+}
+const deviceWidth = Dimensions.get('window').width;
